6685c380f788998f0443d94d295537071c14d34b
TomScott <contact@tascott.co.uk>
2023-01-23 15:24:54 +0000

    Disable save button unless there is text in the box, update present/past colors

--- scripts/index.js
+++ scripts/index.js
@@ -18,6 +18,8 @@ $('#currentDay').text(today)
 //Get current time
 let timeNow =  DateTime.now().toLocaleString(DateTime.TIME_SIMPLE)
 
+// on page load or save update every hour slot with data from local storage
+let dayData = [];
 
 // Refresh when times changed
 $('#startHour, #endHour').on('change', function() {
@@ -32,20 +34,20 @@ $('#startHour, #endHour').on('change', function() {
 // The base HTML structure of each hour in our calendar
 let renderHour = function(hour) {
     let ampm = hour < 12 ? 'am' : 'pm';
-    hour = hour % 12;
-    hour = hour ? hour : 12;
+    let formatHour = hour % 12;
+    formatHour = formatHour ? formatHour : 12;
 
     let startTime = DateTime.fromObject({hour: hour, minute: 0, second: 0, millisecond: 0});
     let endTime = DateTime.fromObject({hour: hour, minute: 59, second: 59, millisecond: 999});
 
-    return `<div class="hour" data-start=${startTime} data-end=${endTime}>
+    return `<div class="hour" data-hour="${hour}" data-period="past" data-start=${startTime} data-end=${endTime}>
                 <div class="label">
-                    <span>${hour}${ampm}</span>
+                    <span>${formatHour}${ampm}</span>
                 </div>
                 <div class="content">
                     <textarea class="hour-text"></textarea>
                 </div>
-                <button class="saveBtn"><i class="fas fa-save"></i></button>
+                <button class="saveBtn" disabled><i class="fas fa-save"></i></button>
             </div>`
 }
 
@@ -64,27 +66,99 @@ function createCalendar() {
     }
 
     updateHoursFromLocalStorage();
+    updateColors();
 }
 
-// On clicking save, grab all the data available and save it to local storage
+//if the textarea is not empty, make saveBtn active
+$('.container').on('keyup', '.hour-text', function() {
+    let text = $(this).val();
+    let saveBtn = $(this).parents().siblings('.saveBtn');
+    if (text) {
+        saveBtn.prop('disabled', false);
+    } else {
+        saveBtn.prop('disabled', true);
+    }
+});
 
+
+// On clicking save, grab all the data available and save it to local storage
 $('.container').on('click', '.saveBtn', function() {
     let hour = $(this).parent().data('start');
     let text = $(this).siblings('.content').children('.hour-text').val();
-    console.log(hour, text)
-    localStorage.setItem(hour, text);
-})
+    let thisHour = {time: hour, text: text};
 
+    dayData = getFromLocalStorage() || [];
 
-createCalendar();
+    if (dayData.length === 0) {
+        dayData.push(thisHour);
+    } else {
+        let found = false;
+        for (let i = 0; i < dayData.length; i++) {
+            if (dayData[i].time === hour) {
+                dayData[i].text = text;
+                found = true;
+            }
+        }
+        if (!found) {
+            dayData.push(thisHour);
+        }
+    }
+
+    $(this).prop('disabled', true);
+
+    saveToLocalStorage(dayData);
+});
 
 let saveToLocalStorage = function(dayData) {
     localStorage.setItem('calendarData', JSON.stringify(dayData));
 }
 
+let getFromLocalStorage = function(){
+    let data = JSON.parse(localStorage.getItem('calendarData'));
+    return data;
+}
 
+let updateHoursFromLocalStorage = function() {
+    let data = getFromLocalStorage();
+    // Loop through each hour in a day and update from localstorage if data matches
+    if (data) {
+        for (let i = 0; i < data.length; i++) {
+            let hour = data[i].time;
+            let text = data[i].text;
+            let hourDiv = $(`[data-start="${hour}"]`);
+            hourDiv.find('.hour-text').val(text);
+        }
+    }
+}
+
+let updateColors = function() {
+    // Get current time and update the colors of the hours
+    now = DateTime.now();
+    let currentHour = now.hour;
+    console.log(currentHour)
+    // loop over all the hours in the container and see which ones matches the current hour
+    $('.hour').each(function() {
+        let hour = $(this).data('hour');
+        if (hour === currentHour) {
+            $(this).attr('data-period', 'present');
+        } else if (hour < currentHour) {
+            $(this).attr('data-period', 'past');
+        } else if (hour > currentHour) {
+            $(this).attr('data-period', 'future');
+        }
+    });
+}
+
+
+createCalendar();
 
 // Loop and run the update function every second, need to find a way to make sure we don't clear stuff when entering - maybe a flag?
 
 
-// When the clock ticks over to the next hour, update the calendar
+// Find out how many seconds are left in the current minute so we can update on the minute
+var time = now;
+var secondsRemaining = (60 - time.second) * 1000;
+
+setTimeout(function() {
+    setInterval(updateColors, 60000);
+}, secondsRemaining);
